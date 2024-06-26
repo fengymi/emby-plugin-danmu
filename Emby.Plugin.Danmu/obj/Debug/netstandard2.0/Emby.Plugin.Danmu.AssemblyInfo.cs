f7a91f27a779fe1861630f7fa85fd7b7a7b3197b
fengymi <yufeng.fym@alibaba-inc.com>
2024-06-26 16:52:33 +0800

    先取出B站获取弹幕

--- Emby.Plugin.Danmu/obj/Debug/netstandard2.0/Emby.Plugin.Danmu.AssemblyInfo.cs
+++ Emby.Plugin.Danmu/obj/Debug/netstandard2.0/Emby.Plugin.Danmu.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("弹幕插件")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f4c364fb4db15879c92d110ee7e0e64f19230f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9eed66fda69ff2abba2c2720e7e98597cbc59a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Emby.Plugin.Danmu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Emby.Plugin.Danmu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
